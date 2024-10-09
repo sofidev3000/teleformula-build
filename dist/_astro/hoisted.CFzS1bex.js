6786ed0f7df8ebb6aa06baad97d7cc950fc136ff
Sofia osorio <165848023+sofidev3000@users.noreply.github.com>
2024-10-09 13:52:59 -0600

    Add files via upload

---
+++ dist/_astro/hoisted.CFzS1bex.js
@@ -0,0 +1,1 @@
+import"https://code.iconify.design/iconify-icon/2.0.0/iconify-icon.min.js";const t=new Date,e={day:"numeric",month:"long",year:"numeric"},o=t.toLocaleString("es-ES",e);document.querySelector(".header__date").textContent=o;
